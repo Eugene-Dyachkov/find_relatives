5ff840a4a1ef152d02cba7204d92a2f45cb6dac7
Eugene-Manjaro <eugene.dyachkov27@gmail.com>
2023-04-15 20:59:52 +0300

    Сделал основу

---
+++ scr/models.py
@@ -0,0 +1,43 @@
+
+
+from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, Date, Table
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship, declarative_base
+
+import uuid
+
+
+
+Base = declarative_base()
+
+users_relatives = Table("users_relatives", Base.metadata,
+    Column('user_id', UUID(as_uuid=True), ForeignKey("user.id")),
+    Column('relatives_id', UUID(as_uuid=True), ForeignKey("relatives.id"))
+
+)
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    username = Column(String(128), nullable=False)
+    email = Column(String(64), unique=True, nullable=False)
+    hashed_password = Column(String(128), nullable=False, index=True)
+    active = Column(Boolean(), nullable=False, default=False)
+    telegram = Column(String(128), nullable=True)
+    vk = Column(String(128), nullable=True)
+    method = Column(String(128), nullable=True)
+
+
+class Relatives(Base):
+    __tablename__ = "relatives"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    surname = Column(String(128), nullable=True)
+    birth_data = Column(Date(), nullable=True)
+    death_data = Column(Date(), nullable=True)
+    sity = Column(String(128), nullable=False)
+    photo = Column(String(128), nullable=True)
+    user = relationship("User", secondary=users_relatives, backref="relatives")
